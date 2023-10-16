75e79b0abcd805d4f3f58c43892d1b297d5a2920
Qii <57295427+syauqqii@users.noreply.github.com>
2023-10-16 19:20:29 +0700

    Update main.py
    optimasi kode

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
+from utils.banner import Banner
+from utils.envLoaders import EnvLoaders
+from utils.helpers import Helpers
+from controllers.authentication import Authentication
+from models.buttons import Buttons
+from models.data import Data
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from requests import Session
-from bs4 import BeautifulSoup
 from json import loads
 import pandas as pd
-from os import system
-from env import USERNAME, PASSWORD, NAMA_FILE, LENGTH, URL_ENRICHMENT_APPS, URL_STUDENT_JOBS_TABLE
 import openpyxl
-from config.init_buttons import LoginPageElements
-from config.get_cookies import LoginProcess
 
-def banner():
-	text = '''
-   /$$$$$$                                          /$$$$$$           
-  /$$__  $$                                        /$$__  $$          
- | $$  \\__/  /$$$$$$$  /$$$$$$  /$$$$$$   /$$$$$$ |__/  \\ $$  /$$$$$$ 
- |  $$$$$$  /$$_____/ /$$__  $$|____  $$ /$$__  $$   /$$$$$/ /$$__  $$
-  \\____  $$| $$      | $$  \\__/ /$$$$$$$| $$  \\ $$  |___  $$| $$  \\__/
-  /$$  \\ $$| $$      | $$      /$$__  $$| $$  | $$ /$$  \\ $$| $$      
- |  $$$$$$/|  $$$$$$$| $$     |  $$$$$$$| $$$$$$$/|  $$$$$$/| $$      
-  \\______/  \\_______/|__/      \\_______/| $$____/  \\______/ |__/      
-                                        | $$                          
-         [ 0xd1m5@gmail.com ]           | $$                   
-               [ v0.1 ]                 |__/
-'''
-	print(text)
+# Load ENV
+env = EnvLoaders(".env")
+
+# Memanggil function get_x (return type: text) pada class EnvLoader yang ada di utils
+USERNAME = env.get_username()
+PASSWORD = env.get_password()
+GUI_MODE = env.get_gui_mode()
+
+NAMA_FILE = env.get_file_name()
+
+LOGIN_URL_ENRICHMENT_APP = env.get_url_enrichment()
+URL_STUDENT_JOBS_TABLE = env.get_url_table_jobs()
+
+DATA = Data().get_data()
 
 def main():
-	system("cls||clear")
+	# Memanggil function clear_screen pada class Helpers yang ada di utils
+	Helpers().clear_screen()
 
-	banner()
+	if int(GUI_MODE) == 0:
+		print(Banner("OFF").print_banner())
+		firefox_options = Options()
+		firefox_options.add_argument('--headless')
 
-	firefox_options = Options()
-	firefox_options.add_argument('--headless')
+		driver = webdriver.Firefox(options=firefox_options)
+	elif int(GUI_MODE) == 1:
+		print(Banner("ON").print_banner())
+		driver = webdriver.Firefox()
+	else:
+		print(" [!] Set GUI_MODE di .env anda antara 0/1.")
+		exit(1)
 
-	driver = webdriver.Firefox(options=firefox_options)
+	login_page = Buttons(driver)
 
-	login_page = LoginPageElements(driver)
+	login_process = Authentication(driver, login_page)
+	login_process.login(LOGIN_URL_ENRICHMENT_APP, USERNAME, PASSWORD)
 
-	login_process = LoginProcess(driver, login_page)
-	login_process.login(URL_ENRICHMENT_APPS, f"{USERNAME}@binus.ac.id", PASSWORD)
-
-	session_cookie = login_process.get_session_cookie()
+	session_cookie = login_process.get_cookie()
 	print(" [+] berhasil mendapatkan cookie")
 
 	driver.quit()
@@ -54,36 +62,8 @@ def main():
 
 	print(" [+] berhasil set cookie")
 
-	data = {
-	    'draw': '2',
-	    'start': '0',
-	    'length': f'{LENGTH}',
-	    'columns[0][data]': '',
-	    'columns[0][name]': '',
-	    'columns[0][searchable]': 'true',
-	    'columns[0][orderable]': 'true',
-	    'columns[0][search][value]': '',
-	    'columns[0][search][regex]': 'false',
-	    'columns[1][data]': '',
-	    'columns[1][name]': '',
-	    'columns[1][searchable]': 'true',
-	    'columns[1][orderable]': 'true',
-	    'columns[1][search][value]': '',
-	    'columns[1][search][regex]': 'false',
-	    'columns[2][data]': '',
-	    'columns[2][name]': '',
-	    'columns[2][searchable]': 'true',
-	    'columns[2][orderable]': 'true',
-	    'columns[2][search][value]': '',
-	    'columns[2][search][regex]': 'false',
-	    'search[value]': '',
-	    'search[regex]': 'false',
-	    'order[0][column]': '0',
-	    'order[0][dir]': 'asc'
-	}
-
 	print(" [+] proses request data job")
-	response = session.post(URL_STUDENT_JOBS_TABLE, data=data)
+	response = session.post(URL_STUDENT_JOBS_TABLE, data=DATA)
 
 	if response.status_code == 200:
 		print(" [+] scraping berhasil")
